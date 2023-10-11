14a72fb3e44bb9f8afd7c33769ca22fe72dbcfa7
trantuvan <trantuvan.kan@gmail.com>
2023-10-11 11:00:31 +0700

    BinarySearchList
    Summary:
    + Use binarySearch when the list is already sorted
    + Search 1/2 of the list | binary 1-0
    + Big0 O(logN)
    
    Action:
    + Calculate lo, hi
    + Calculate mid point
    + Check needle val equal midVal
    + Check needle val > midVal -> update lo
    + Check needle val < midVal -> update hi

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        const mid = Math.floor(lo + (hi - lo) / 2);
+        const midVal = haystack[mid];
+
+        if (midVal === needle) {
+            return true;
+        } else if (midVal > needle) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    } while (lo < hi);
+
+    return false;
 }

78111dbaad99608fa6efd63a1f57be38b820fd3f
trantuvan <trantuvan.kan@gmail.com>
2023-10-12 19:36:26 +0700

    TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumps = Math.floor(Math.sqrt(breaks.length));
+    let jumpsIdx = jumps;
 
+    //* Check if jumpsIdx is break position
+    for (; jumpsIdx < breaks.length; jumpsIdx += jumps) {
+        if (breaks[jumpsIdx] === true) {
+            break;
+        }
+    }
+
+    jumpsIdx -= jumps;
+    //* walk from 0 - first breaks idx to jumpsIdx
+    //* search 1st break position
+    for (let j = 0; j <= jumps && jumpsIdx < breaks.length; j++, jumpsIdx++) {
+        if (breaks[jumpsIdx] === true) {
+            return jumpsIdx;
+        }
+    }
+
+    return -1;
 }

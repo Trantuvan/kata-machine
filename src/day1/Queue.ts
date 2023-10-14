1f8c61095752f96cdc967ee41d6d16947259b3d7
trantuvan <trantuvan.kan@gmail.com>
2023-10-14 15:51:25 +0700

    queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,53 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        this.length++;
+        const node = { value: item } as Node<T>;
 
-}
+        //* empty queue ? -> create new node
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
+        //* const head also a node has next
+        const head = this.head;
+        this.head = this.head.next;
 
-}
+        //* free memory const head
+        head.next = undefined;
+
+        //? tail if length === 0
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
